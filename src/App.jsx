f7f9e384a22004ae2ad49173833b213fadbee017
shahriaraf <shahriaraf01@gamil.com>
2025-08-05 16:33:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './Home'
+
+function App() {
+
+  return (
+    <>
+    <Home></Home>
+
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p className='text-6xl text-green-700'> This is Home</p>
+        </div>
+    );
+};
+
+export default Home;
